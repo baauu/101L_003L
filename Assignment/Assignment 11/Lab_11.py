15053eb164509db3170ce769330546cc5a7e624e
baauu <jbr75@umsystem.edu>
2021-11-19 21:21:31 -0600

    updated lab11.py

---
+++ Assignment/Assignment 11/Lab_11.py
@@ -0,0 +1,61 @@
+##########################################
+##
+## Jessi Bautista Espino
+## jbr75@umsystem.edu 
+## CS 101 Lab
+## Program 11
+##
+##########################################
+import time
+class clock:
+    def __init__(self, hour, minu, sec, clock_type = 0):
+        self.hr = hour
+        self.min = minu
+        self.sec = sec
+        self.clock_type = clock_type
+        
+    def tick(self):
+        self.sec += 1
+        if self.sec > 59:
+            self.min += 1
+            self.sec = 0
+        if self.min > 59:
+            self.hr += 1
+            self.min = 0
+        if self.hr > 24:
+            self.hr = 0 
+
+    def __str__(self):
+        if self.clock_type == 0:
+            return ("{:02d}:{:02d}:{:02d}".format(self.hr, self.min, self.sec))
+        elif self.clock_type == 1:
+            time_letters = "AM"
+            if self.hr > 12:
+                time_letters = "PM"
+                if self.hr < 0:
+                    pass
+                else:
+                    self.hr = self.hr - 12
+            return ("{:02d}:{:02d}:{:02d} {}".format(self.hr, self.min, self.sec, time_letters))
+    
+
+
+if __name__ == "__main__":
+    hour = int(input("Enter hour: "))
+    minute = int(input("Enter minutes: "))
+    second = int(input("Enter Seconds: "))
+    c_type = int(input("24hr clock = 0, AM/PM = 1: "))
+
+    output = clock(hour, minute, second, c_type)
+    '''print(output)'''
+
+    while True:
+        print(output)
+        output.tick()
+        '''print(output)'''
+        time.sleep(1) 
+
+
+    
+
+
